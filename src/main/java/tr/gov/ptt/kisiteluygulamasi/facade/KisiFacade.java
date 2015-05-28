4805435bbcb184d7e849e9cf3527cfbd93dcefcd
Administrator <Administrator@10.10.10.57>
2015-05-28 11:53:12 +0300

    entity ve facade eklendi.

---
+++ src/main/java/tr/gov/ptt/kisiteluygulamasi/facade/KisiFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tr.gov.ptt.kisiteluygulamasi.facade;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import tr.gov.ptt.kisiteluygulamasi.entity.Kisi;
+
+/**
+ *
+ * @author Administrator
+ */
+@Stateless
+public class KisiFacade extends AbstractFacade<Kisi> {
+    @PersistenceContext(unitName = "KisiTelUygulamasiPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public KisiFacade() {
+        super(Kisi.class);
+    }
+    
+}
